5168b67b308daddb7c3134a599967ff6e626d750
anthony-tricarico <tricarico672@gmail.com>
2026-01-07 20:27:53 +0100

    feat: added dataset reader to mpi implementation

--- mpi-implementation/src/main.c
+++ mpi-implementation/src/main.c
@@ -4,6 +4,7 @@
 #include <stdlib.h>
 #include "../include/tree_reduce.h"
 #include "../../include/memory_utils.h"
+#include "../../include/dataset_reader.h"
 
 
 /* NOTE: These are test parameters and should be removed in 
@@ -15,27 +16,46 @@
 #define UPPER_BOUND 40
 #define K 200
 
-
-/* ============== MAIN FUNCTION ================ */
-int main(void) 
+int main(int argc, char **argv) 
 {
+    if (argc != 2) {
+        fprintf(stderr, "Usage: %s <file_path>\n", argv[0]);
+        exit(EXIT_FAILURE);
+    }
+
     int rank, comm_sz;
+    int count_out = 0; 
     double local_start, local_finish, local_elapsed, elapsed;
 
-    MPI_Init(NULL, NULL);
+    MPI_Init(&argc, &argv);
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
     MPI_Comm_size(MPI_COMM_WORLD, &comm_sz);
 
-    /* Handling possible rest division in n/comm_sz */
-    int base = DATA_SIZE/comm_sz;
-    int rest = DATA_SIZE % comm_sz;
+    int total_data_size = 0;
+    int *buf = NULL;
 
-    /* Filling counts and displacements arrys with counts of 
-    how many integers each nodes will receive and with offesets
-    describing how to scatter the original array into the 
-    different processes. */
-    int *counts = xmalloc(comm_sz*sizeof(int));
-    int *displs = xmalloc(comm_sz*sizeof(int));
+    if (rank == 0) {
+        printf("[rank %d] Reading from file: %s\n", rank, argv[1]);
+
+        buf = read_ints_from_file(argv[1], &count_out);
+        
+        if (buf == NULL) {
+            fprintf(stderr, "Error: Could not read file.\n");
+            MPI_Abort(MPI_COMM_WORLD, 1);
+        }
+
+        total_data_size = count_out;
+        printf("First entry: %d\n", buf[0]);
+        printf("Total entries read: %d\n", total_data_size);
+    }
+
+    MPI_Bcast(&total_data_size, 1, MPI_INT, 0, MPI_COMM_WORLD);
+
+    int base = total_data_size / comm_sz;
+    int rest = total_data_size % comm_sz;
+
+    int *counts = xmalloc(comm_sz * sizeof(int));
+    int *displs = xmalloc(comm_sz * sizeof(int));
     int offset = 0;
     for (int i = 0; i < comm_sz; i++) {
         counts[i] = base + (i < rest ? 1 : 0);
@@ -43,28 +63,10 @@ int main(void)
         offset += counts[i];
     }
 
-    /* Computing local dimension of the array for the given rank
-    and allocating proper memory space to held the scattered data. */
     int local_n = counts[rank];
-    int *local_buf = xmalloc(local_n*sizeof(int));
-
-    /* Scatter the array around the nodes */ 
-    if (rank == 0) {
-        printf("[rank %d] starting data distribution\n", rank);
-    }
-    // MPI_Barrier(MPI_COMM_WORLD); // DEBUGGING
-    /* This is initializing the array, technically only rank 0
-     * should initialize the array and scatter it around */
-    
-    int *buf = NULL;
-    if (rank == 0) {
-        buf = xmalloc(DATA_SIZE * sizeof(int));
-        initialize_data_array(rank, buf, DATA_SIZE, LOWER_BOUND, UPPER_BOUND);
-    }
+    int *local_buf = xmalloc(local_n * sizeof(int));
 
     // ============ Start timing ==============
-
-    // Sync processes and start timing
     MPI_Barrier(MPI_COMM_WORLD); 
     local_start = MPI_Wtime();
 
@@ -75,50 +77,44 @@ int main(void)
         displs,
         local_n,
         rank,
-        DATA_SIZE,
+        total_data_size, 
         MPI_COMM_WORLD
     );
+
     if (rank == 0) {
         printf("[rank %d] Data distributed correctly!\n", rank);
+        free(buf); // Rank 0 can free the big buffer now, it's been scattered
+    } else {
+        printf("[rank %d] Gathered %d data points\n", rank, local_n);
     }
-    // printf("[rank %d] finished scatter, building local digest\n", rank);
-    // MPI_Barrier(MPI_COMM_WORLD); // DEBUGGING 
-    // From the data buffer create the q-digesti
+
     size_t local_upper_bound = _get_curr_upper_bound(local_buf, local_n);
     size_t global_upper_bound;
-    MPI_Allreduce(
-        &local_upper_bound,
-        &global_upper_bound,
-        1,
-        MPI_UNSIGNED_LONG,
-        MPI_MAX,
-        MPI_COMM_WORLD
-    );
-    struct QDigest *q = _build_q_from_vector(local_buf, local_n, global_upper_bound, K);
-    // printf("[rank %d] built q-digest, starting tree_reduce\n", rank);
-    // MPI_Barrier(MPI_COMM_WORLD); // DEBUGGING 
+    MPI_Allreduce(&local_upper_bound, &global_upper_bound, 1, MPI_UNSIGNED_LONG, MPI_MAX, MPI_COMM_WORLD);
 
-    // data get inserted into qdigest and then compressed, ecc...
+    struct QDigest *q = _build_q_from_vector(local_buf, local_n, global_upper_bound, K);
+    
     tree_reduce(q, comm_sz, rank, MPI_COMM_WORLD);
-    // printf("[rank %d] tree_reduce completed\n", rank);
-    // MPI_Barrier(MPI_COMM_WORLD); // DEBUGGING 
 
     local_finish = MPI_Wtime();
-
     // ============ End timing ==============
 
     local_elapsed = local_finish - local_start;
-    MPI_Reduce(&local_elapsed, &elapsed, 1, MPI_DOUBLE,
-            MPI_MAX, 0, MPI_COMM_WORLD);
+    MPI_Reduce(&local_elapsed, &elapsed, 1, MPI_DOUBLE, MPI_MAX, 0, MPI_COMM_WORLD);
 
-    if (rank == 0) 
+    // TODO: decide if remove MPI_Barrier, I added it because I wanted to make sure that
+    // the final reduced tree was completed.
+    MPI_Barrier(MPI_COMM_WORLD);
+    // TODO: decide if percentile computation should be considered as part of the timed portion!
+    if (rank == 0) {
         printf("[rank %d] Elapsed time = %e seconds\n", rank, elapsed);
+        printf("The median of the distribution is %zu\n", percentile(q, 0.5));
+    }
 
     free(counts);
     free(displs);
     free(local_buf);
-    free(buf);
-    MPI_Finalize();
     
+    MPI_Finalize();
     return 0;
 }
